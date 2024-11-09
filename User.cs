acee7fee0ed615db7df3e7605c1996b9f7caa56f
Mongezi2003 <131520373+Mongezi2003@users.noreply.github.com>
2024-11-09 16:10:34 +0200

    User.cs
    This model manages user roles and information.

---
+++ User.cs
@@ -0,0 +1,6 @@
+public class User
+{
+    public int UserID { get; set; }
+    public string Username { get; set; }
+    public string Role { get; set; } // E.g., Lecturer, ProgrammeCoordinator, AcademicManager, HR
+}
